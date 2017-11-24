98a2c6a009b5cfba799f390b29c8584e8a3cb07e
farland <farland@163.com>
2017-11-24 22:36:10 +0800

    :xx

---
+++ js/math.js
@@ -0,0 +1,8 @@
+MathJax.Hub.Config({
+  config: ["MMLorHTML.js"],
+  inlineMath: [['$','$'], ['\\(','\\)']],
+  jax: ["input/TeX", "output/HTML-CSS", "output/NativeMML"],
+  extensions: ["MathMenu.js", "MathZoom.js"],
+  enable_dollar_delimiter:true
+});
+
